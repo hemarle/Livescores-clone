d2bbbc1649c05845d3a2bdc196d72dc4784ff388
Daramola Oluwafemi <dfmopeolwa@gmail.com>
2021-01-31 21:06:36 +0100

    Fixed desktop fonts

--- src/RightbarOption.js
+++ src/RightbarOption.js
@@ -1,38 +1,27 @@
 import React, { useEffect } from "react";
-import "./RightBarOption.css";
-import db from "./firebase";
-import { actionTypes } from "./reducer.js";
+import "./RightbarOption.css";
 
-import { useStateValue } from "./StateProvider";
 function RightbarOption({ url, data, tablename }) {
-  //   const [{ EPL, UEFA }, dispatc] = useStateValue();
-
-  // console.log("EPL", EPL);
-
-  // console.log("uefa", UEFA);
-  // console.log("lives", LIVES);
-  // console.log("table", table);
-  // console.log("fixture", fixture);
   return (
     <div className="rightbarOption">
-      <div className="rightbar__ads">{/*Place Your ads here*/}</div>
-      <div className="rightbar__title">LiveScore Table</div>
-      <div className="rightbar__table">
-        <div className="rightbar__tableTitle">
-          <h6 className="rightbar__tableTitleLeague">
+      <div className="rightbarOption__ads">{/*Place Your ads here*/}</div>
+      <div className="rightbarOption__title">LiveScore Table</div>
+      <div className="rightbarOption__table">
+        <div className="rightbarOption__tableTitle">
+          <div className="rightbarOption__tableTitleLeague">
             {tablename ? tablename : "Premier League"}
-          </h6>
-          <h6 className="rightbar__tableTitleP">P</h6>
-          <h6 className="rightbar__tableTitlePTS">PTS</h6>
+          </div>
+          <div className="rightbarOption__tableTitleP">P</div>
+          <div className="rightbarOption__tableTitlePTS">PTS</div>
         </div>
 
         {data &&
           data.table?.map((tab) => (
-            <div className="rightbar__tableBody">
-              <div className="rightbar__position">{tab.rank}</div>
-              <div className="rightbar__club">{tab.name}</div>
-              <div className="rightbar__played">{tab.matches}</div>
-              <div className="rightbar__points">{tab.points}</div>
+            <div className="rightbarOption__tableBody">
+              <div className="rightbarOption__position">{tab.rank}</div>
+              <div className="rightbarOption__club">{tab.name}</div>
+              <div className="rightbarOption__played">{tab.matches}</div>
+              <div className="rightbarOption__points">{tab.points}</div>
             </div>
           ))}
       </div>
